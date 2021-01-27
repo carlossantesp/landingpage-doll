9d613e9be531b4075da80dc6554ee952fdc7baa8
Carlos Santillan Espinoza <carlos.santesp@gmail.com>
2021-01-26 20:52:51 -0400

    Agregando smooth scroll a los enlaces

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa'
 import { FooterContainer, FooterLink, FooterLinkItems, FooterLinksContainer, FooterLinksWrapper, FooterLinkTitle, FooterWrap, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap, WebsiteRights } from './FooterElements'
 
+import { animateScroll as scroll } from 'react-scroll'
+
 const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  }
+
   return (
     <FooterContainer>
       <FooterWrap>
@@ -43,7 +49,7 @@ const Footer = () => {
         </FooterLinksContainer>
         <SocialMedia>
           <SocialMediaWrap>
-            <SocialLogo to="/">
+            <SocialLogo to="/" onClick={toggleHome}>
               dev
             </SocialLogo>
             <WebsiteRights>dev &copy; { new Date().getFullYear() } All rights reserved/</WebsiteRights>
